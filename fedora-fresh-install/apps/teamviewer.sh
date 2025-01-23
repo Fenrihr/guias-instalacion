77dd7c37bfca12ecd3e37b36ef46bfd580fd1daa
Francisco <f.vivamayer1852@gmail.com>
2025-01-23 11:57:28 -0300

    Update teamviewer.sh

--- fedora-fresh-install/apps/teamviewer.sh
+++ fedora-fresh-install/apps/teamviewer.sh
@@ -3,4 +3,11 @@
 if ! [ -f "teamviewer.x86_64.rpm" ] ; then
     wget https://download.teamviewer.com/download/linux/teamviewer.x86_64.rpm
 fi
+
 sudo dnf -y install ./teamviewer.x86_64.rpm
+
+# Limpiamos
+rm teamviewer.x86_64.rpm
+
+# Actualizamos
+sudo dnf update
