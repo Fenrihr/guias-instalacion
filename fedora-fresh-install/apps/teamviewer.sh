ac6932c1bce19ce11b44c9067f473ea4c2bbd7ca
Francisco <git@fmvm.com.ar>
2025-01-30 11:16:28 -0300

    Update teamviewer.sh

--- fedora-fresh-install/apps/teamviewer.sh
+++ fedora-fresh-install/apps/teamviewer.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo -e "\e[1;32m\n[INFO] - Instalando TeamViewer.\n\e[0m"
+sleep 5
 
 if ! [ -f "teamviewer.x86_64.rpm" ] ; then
     wget https://download.teamviewer.com/download/linux/teamviewer.x86_64.rpm
