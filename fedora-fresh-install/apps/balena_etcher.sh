ad71dbc30fe740c23acd7ddd296f168c7b5b9a0a
Francisco <git@fmvm.com.ar>
2025-01-30 11:16:20 -0300

    Update balena_etcher.sh

--- fedora-fresh-install/apps/balena_etcher.sh
+++ fedora-fresh-install/apps/balena_etcher.sh
@@ -4,6 +4,9 @@
 VERSION='1.19.25'
 RELEASE='1.19.25-1'
 
+echo -e "\e[1;32m\n[INFO] - Instalando Balena Etcher ${RELEASE} .\n\e[0m"
+sleep 5
+
 #curl -L -O https://github.com/balena-io/etcher/releases/download/v1.19.25/balena-etcher-1.19.25-1.x86_64.rpm
 wget https://github.com/balena-io/etcher/releases/download/v${VERSION}/balena-etcher-${RELEASE}.x86_64.rpm
 
