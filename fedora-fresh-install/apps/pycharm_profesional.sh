93bed290643132d945af9e1a889d8e5a2da46dfd
Francisco <git@fmvm.com.ar>
2025-02-05 15:34:03 -0300

    update pycharm_profesional.sh

--- fedora-fresh-install/apps/pycharm_profesional.sh
+++ fedora-fresh-install/apps/pycharm_profesional.sh
@@ -2,10 +2,13 @@
 
 # Guía de instalación: https://www.jetbrains.com/help/pycharm/installation-guide.html#toolbox
 
-echo -e "\e[1;32m\n[INFO] - Instalando PyCharm Profesional.\n\e[0m"
+echo -e "
+\e[1;32m\n[INFO] - Instalando PyCharm Profesional.\n\e[0m"
 sleep 5
 
+# https://snapcraft.io/docs/installing-snap-on-fedora
 sudo dnf install -y snapd
+sudo ln -s /var/lib/snapd/snap /snap
 
 sudo snap install -y pycharm-professional --classic
 
